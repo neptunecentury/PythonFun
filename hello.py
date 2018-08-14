7c94e4e7661bd35134578cce79043517da76f3db
Eric Butler <ebutler@milestechnologies.com>
2018-08-14 13:11:06 -0400

    Initial commit

---
+++ hello.py
@@ -0,0 +1,18 @@
+
+someNames = ['Apple', 'Car']
+for name in someNames:
+    print(name)
+
+# This is a comment. Ask the user which is their favorite
+fav = input("Which one is your favorite? ")
+# Conver the user's response to lower case so that it is easy
+# to test what they said.
+fav = fav.lower()
+# The below code checks what your favorite is and then responds.
+if fav == 'apple':
+    print("Hey! That's my favorite too! \\o/")
+    print("You're the best!")
+elif fav == 'car':
+    print("Cars are cool, I guess...")
+else:
+    print("Sorry, I don't understand.")
