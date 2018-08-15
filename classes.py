9010e9a2c6d2188f01b49762acb4df3dbfd6b52b
Eric Butler <ebutler@milestechnologies.com>
2018-08-15 16:06:14 -0400

    Class example

---
+++ classes.py
@@ -0,0 +1,51 @@
+# A class is a self contained object that can have properties and functions. You can create
+# as many instances of the class as you want and assign different attributes to each one.
+
+class Cat:
+    # We are just defining some variables here, but they have no value, hence None
+    # The name of our cat
+    name = None
+    # The color of our cat
+    color = None
+
+    # Define an initializer method. This method is called automatically when we create
+    # a new instance of a Cat. This method has an optional parameter called color. If
+    # we don't pass it, it will default to Black
+    def __init__(self, name, color="Black"):
+        # self is the variable that contains the information in this class. So,
+        # if we want to populate some default values, we can do that here.
+        self.name = name
+        self.color = color
+        
+
+    # Let's define a pur method. Methods inside classes pass in a default parameter called self.
+    # this let's the function access variables in the class
+    def pur(self):
+        print("{0} says purrrrrrrrrrrrrrrrrrrrr".format(self.name))
+
+    # Let's define a meow method
+    def meow(self):
+        print("{0} says meow".format(self.name))
+#End Class - I miss code blocks :(
+
+# OK, now lets create a cat and give it a name!
+# Since this __init__ method has an optional parameter called color. We
+# don't have to specify the color.
+myCat = Cat("Meowster")
+
+# The cat can pur and meow. We call a method like so:
+myCat.pur()
+myCat.meow()
+
+# What color is the cat?
+print("{0} is a {1} cat.".format(myCat.name, myCat.color))
+
+# OK, now lets create another cat and give it a name and a paint job!
+myOtherCat = Cat("Big Fluffy", "Pink")
+
+# The cat can pur and meow
+myOtherCat.pur()
+myOtherCat.meow()
+
+# What color is the cat?
+print("{0} is a {1} cat.".format(myOtherCat.name, myOtherCat.color))
