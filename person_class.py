44a3db7c00c66281e10655f66894bd5cc6bc7382
Eric Butler <ebutler@milestechnologies.com>
2018-08-17 08:10:36 -0400

    Added script include example

---
+++ person_class.py
@@ -0,0 +1,15 @@
+# Define our person class
+class Person:
+    name = None
+
+    # Create a constructor method. Remember, this is called automatically when
+    # a new person is created.
+    def __init__(self, name):
+        self.name = name
+
+    # A person can speak some words
+    def speak(self, words):
+        # Say something!. If you want to include quote characters in your
+        # string, you'll need to escape them with a \, or else the interpreter
+        # will think you are closing the string.
+        print("{0} says \"{1}\"".format(self.name, words))
---
+++ person_class_example.py
@@ -0,0 +1,14 @@
+# Here, we will import the Person class from the person_class file. If we had multuple things
+# to import in the person_class file, we could use import *, but this should be used with caution-You
+# might end up importing two methods or classes with the same name, and one would overwrite the other.
+from person_class import Person
+
+def run():
+    # Make a new person
+    frank = Person("Frank")
+    # Make frank say something
+    frank.speak("Hi, I'm an instance of a Person object. Whoo hooo!")
+
+
+# Run the app
+run()
