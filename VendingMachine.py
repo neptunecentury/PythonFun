cce3722658df9415ab41e91a05926a97285f30be
Eric Butler <ebutler@milestechnologies.com>
2018-08-27 08:55:35 -0400

    vending machine class

--- VendingMachine.py
+++ VendingMachine.py
@@ -1,6 +1,31 @@
 # In this example, we will reference an item in the array by its index. An index is the position the
 # item is in, 0 being the first index.
 
+# VendingMachine class provides the functionality to make selections
+# and insert money and dispense items
+class VendingMachine:
+    # This variable stores how much money the user has available
+    availableFunds = 0
+
+    # Add some money to the vending machine so we can get goodies
+    def AddMoney(self, amount):
+        # Add some money to the vending machine
+        self.availableFunds += amount
+
+    # Start our vending machine and offer selections
+    def Run(self):
+        # In here we need to present the user selection and
+        # provide them the ability to insert money
+
+    def Dispense(self):
+        # Show the user that the item was dispensed and deduct the
+        # price from the available funds. Show user how much money
+        # they have left.
+
+
+
+
+# Candy class
 class Candy:
     name = None
     candyType = None
